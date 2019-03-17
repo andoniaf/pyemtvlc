9baaf150e93f72976db3f9aa38a71a200b15a68d
Andoni A <andonialonsof@gmail.com>
2019-03-17 18:49:26 +0100

    Add some testing

---
+++ test/test_outs.py
@@ -0,0 +1,16 @@
+from subprocess import PIPE, Popen as popen
+from unittest import TestCase
+
+
+class BasicOutputs(TestCase):
+    def test_returns_usage_information(self):
+        output = str(popen(['pyemtvlc'], stdout=PIPE).communicate()[0])
+        self.assertTrue('Uso:' in output)
+
+    def test_returns_parada_echo(self):
+        output = str(popen(['pyemtvlc', '13'], stdout=PIPE).communicate()[0])
+        self.assertTrue('Parada: 13' in output)
+
+    def test_returns_parada_echo(self):
+        output = str(popen(['pyemtvlc', 'abcd'], stdout=PIPE).communicate()[0])
+        self.assertTrue('No has introducido un' in output)
