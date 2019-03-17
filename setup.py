73142b7303019166b49475f2d2c70d2c0651042e
Andoni A <andonialonsof@gmail.com>
2019-03-17 09:22:26 +0100

    Update setup info

--- setup.py
+++ setup.py
@@ -1,39 +1,22 @@
 import setuptools
 
 with open("README.md", "r") as fh:
-
     long_description = fh.read()
 
 setuptools.setup(
-
-     name='dokr',  
-
+     name='pyemtvlc',  
      version='0.1',
-
      scripts=['pyemtvlc'] ,
-
      author="Andoni Alonso F.",
-
      author_email="andonialonsof@gmail.com",
-
      description="Python package to query  EMT Valencia (bus).",
-
      long_description=long_description,
-
      long_description_content_type="text/markdown",
-
      url="https://github.com/andoniaf/pyemtvlc",
-
      packages=setuptools.find_packages(),
-
      classifiers=[
-
          "Programming Language :: Python :: 3",
-
-         "License :: OSI Approved :: GPL-3.0",
-
+         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
          "Operating System :: OS Independent",
-
      ],
-
  )
