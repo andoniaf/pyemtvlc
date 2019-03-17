88b3f249cf5172db0548c2c31e7a6522c9b229a2
Andoni A <andonialonsof@gmail.com>
2019-03-17 10:36:38 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
      name='pyemtvlc',
-     version='0.1.2',
+     version='0.1.3',
      scripts=['pyemtvlc'],
      author="Andoni Alonso F.",
      author_email="andonialonsof@gmail.com",
@@ -15,7 +15,7 @@ setuptools.setup(
      url="https://github.com/andoniaf/pyemtvlc",
      packages=setuptools.find_packages(),
      install_requires=[
-         'requests', 'BeautifulSoup'
+         'requests', 'beautifulsoup4'
      ],
      classifiers=[
          "Programming Language :: Python :: 3",
