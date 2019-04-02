681c9a7652bc0ddb76ef803caf170f158471160e
Andoni Alonso <andonialonsof@gmail.com>
2019-04-02 20:47:23 +0200

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='pyemtvlc',
-    version='0.3.2',
+    version='0.3.3',
     scripts=['pyemtvlc'],
     author="Andoni Alonso F.",
     author_email="andonialonsof@gmail.com",
